e1bff68473f2cecdf2a216a8dba460e47aa165dd
shahery <shaheryar924@gmail.com>
2022-04-07 00:41:25 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+//function addTwoNumbers(a, b){
+    //return a + b;
+//}
+//let sum = addTwoNumbers(4, 5);
+//console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(4, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers1 = (a, b) => a + b;
+let sum1 = addTwoNumbers1(4, 5);
+console.log(sum1);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there')
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+      This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines())
